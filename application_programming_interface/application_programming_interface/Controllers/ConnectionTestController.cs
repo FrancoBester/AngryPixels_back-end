ae2a66b9b0126502e651789c3cdd028e44b3fb5c
Carel Haasbroek <84836107+CarelTransformIt@users.noreply.github.com>
2021-08-28 11:28:11 +0200

    Connection test added

---
+++ application_programming_interface/application_programming_interface/Controllers/ConnectionTestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace application_programming_interface.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConnectionTestController : ControllerBase
+    {
+        // GET: api/<ConnectionTestController>
+        [HttpGet]
+        [Route("~/api/ConnectionTest/test")]
+        public JsonResult Get()
+        {
+            return new JsonResult(new { value = "Sucsess" });
+        }
+
+    }
+}
