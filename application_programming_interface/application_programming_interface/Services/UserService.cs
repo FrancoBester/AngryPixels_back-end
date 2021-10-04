c794f6b36a5f66b7c3dc85f0f0d456b4355e7e0d
Carel Haasbroek <84836107+CarelTransformIt@users.noreply.github.com>
2021-10-04 21:43:21 +0200

    Some Smoll changes

---
+++ application_programming_interface/application_programming_interface/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using application_programming_interface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace application_programming_interface.Services
+{
+    public class UserService: IUserService
+    {
+        //Add user code here
+    }
+}
